b7526d6a3856e4a7976727fa3ba00bec959815ed
CaleboF <cal2152346@maricopa.edu>
2021-10-21 19:55:54 -0700

    Initial commit

---
+++ src/test/java/Bag.java
@@ -0,0 +1,27 @@
+import java.util.ArrayList;
+import java.util.Iterator;
+
+public class Bag<t> implements Container{
+ArrayList<Object> items = new ArrayList<>();
+
+
+    @Override
+    public boolean isEmpty() {
+        return (items.size() == 0);
+    }
+
+    @Override
+    public int size() {
+        return items.size();
+    }
+
+    @Override
+    public void add(Object item) {
+      items.add(item);
+    }
+
+    @Override
+    public Iterator iterator() {
+        return items.iterator();
+    }
+}
